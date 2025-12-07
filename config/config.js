72bd0e19d210e7f3e22647e70f904bf863bec3e0
pankajswamivaishnav <pankajvaishnav128@gmail.com>
2025-12-07 14:02:10 +0530

    untrack env file

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   APP_SECRET: process.env.APP_SECRET,
   EMAIL_USERNAME: process.env.EMAIL_USERNAME,
   EMAIL_PASSWORD: process.env.EMAIL_PASSWORD,
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+  SENDGRID_API_SECRET: process.env.SENDGRID_API_SECRET,
 };
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,38 +1,70 @@
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
+// const ejs = require("ejs");
+// const path = require("path");
+// const sendEmail = async (options) => {
+//   const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+
+//   const templatePath = path.join(__dirname, "../templates/email-template.ejs");
+//   const html = await ejs.renderFile(templatePath, options.templateData || {});
+
+//   const mailOptions = {
+//     from: `"Speed Up" <${process.env.EMAIL_USERNAME}>`,
+//     to: options.email,
+//     subject: options.subject || "Speed Up Notification",
+//     html: html,
+//     // text: options.message || 'Speed Up Notification',
+//   };
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
+
+const sgMail = require("@sendgrid/mail");
 const ejs = require("ejs");
 const path = require("path");
+
+// Set SendGrid API key
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 const sendEmail = async (options) => {
-  // Create Transport
-  // const transporter = nodemailer.createTransport({
-  //   host: "smtp.ethereal.email",
-  //   port: 587,
-  //   secure: false,
-  //   service: "gmail",
-  //   auth: {
-  //     user: process.env.EMAIL_USERNAME,
-  //     pass: process.env.EMAIL_PASSWORD,
-  //   },
-  // });
+  try {
+    // 1. Render EJS template to HTML
+    const templatePath = path.join(
+      __dirname,
+      "../templates/email-template.ejs"
+    );
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+    const html = await ejs.renderFile(templatePath, options.templateData || {});
 
-  const templatePath = path.join(__dirname, "../templates/email-template.ejs");
-  const html = await ejs.renderFile(templatePath, options.templateData || {});
+    // 2. Build email
+    const msg = {
+      from: {
+        email: process.env.EMAIL_FROM, // must be a verified SendGrid sender
+        name: "Speed Up",
+      },
+      to: options.email, // receiver
+      subject: options.subject || "Speed Up Notification",
+      html: html,
+    };
 
-  const mailOptions = {
-    from: `"Speed Up" <${process.env.EMAIL_USERNAME}>`,
-    to: options.email,
-    subject: options.subject || "Speed Up Notification",
-    html: html,
-    // text: options.message || 'Speed Up Notification',
-  };
-  await transporter.sendMail(mailOptions);
+    // 3. Send mail via SendGrid
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error("SendGrid email error:", error);
+
+    if (error.response && error.response.body) {
+      console.error("SendGrid response body:", error.response.body);
+    }
+
+    // rethrow so your catchAsyncHandler / ErrorHandler can catch it
+    throw error;
+  }
 };
 
 module.exports = sendEmail;
